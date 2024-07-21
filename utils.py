9bdbdf7c222d6a77bdc92a865a4dcdd9a7fa4410
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-07-22 02:18:43 +0530

    indent

--- utils.py
+++ utils.py
@@ -102,19 +102,20 @@ async def get_movie_poster(movie_name, release_year):
 
                         async with session.get(tmdb_movie_image_url) as movie_response:
                             movie_images = await movie_response.json()
-
+ 
                         # Use the backdrop_path or poster_path
                             poster_path = None
                             if 'backdrops' in movie_images and movie_images['backdrops']:
                                 poster_path = movie_images['backdrops'][0]['file_path']
-
+                                                        
                             elif 'backdrop_path' in result and result['backdrop_path']:
                                 poster_path = result['backdrop_path']
 
                             elif 'poster_path' in result and result['poster_path']:
-                                poster_path = result['poster_path']                                         
-                                poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
-                                return poster_url
+                                poster_path = result['poster_path']
+
+                            poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
+                            return poster_url
 
     except Exception as e:
         print(f"Error fetching TMDB data: {e}")
