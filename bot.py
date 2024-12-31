b082bb4a92ac4fc6bd7df596b494d6639cc6d9e4
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-12-31 14:39:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,8 @@
 import os
-import aiohttp
 import asyncio
-import aiofiles
 import imgbbpy
 import time
-from os import path as ospath
-from io import BytesIO
 from tzlocal import get_localzone
-from aiofiles import open as aiopen
-from aiofiles.os import remove as aioremove, path as aiopath, mkdir
-from functools import partial
-from aiohttp import ClientSession as aioClientSession
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram import Client, enums, filters, utils as pyroutils
 from pyromod import listen
@@ -20,9 +12,6 @@ from inspect import signature
 from motor.motor_asyncio import AsyncIOMotorClient 
 from html import escape
 from asyncio import Queue
-from concurrent.futures import ThreadPoolExecutor
-
-THREADPOOL = ThreadPoolExecutor(max_workers = 1000)
 
 pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
@@ -76,11 +65,6 @@ user = Client(
 bot_loop = bot.loop
 bot_username = bot.me.username
 AsyncIOScheduler(timezone = str(get_localzone()), event_loop = bot_loop)
-
-async def sync_to_async(func, *args, wait=True, **kwargs):
-    pfunc = partial(func, *args, **kwargs)
-    future = bot_loop.run_in_executor(THREADPOOL, pfunc)
-    return await future if wait else future
 
 @bot.on_message(filters.private & filters.command("start"))
 async def start_command(client, message):
@@ -321,76 +305,6 @@ async def log_command(client, message):
         await auto_delete_message(message, reply)
     except Exception as e:
         await bot.send_message(user_id, f"Failed to send log file. Error: {str(e)}")
-
-@bot.on_message(filters.private & filters.command("thumb") & filters.user(OWNER_ID))
-async def thumb_command(client, message):
-    des_dir = f"thumbnails/thumb.jpg"
-    try:
-        bot_message = await message.reply_text('send thumbnail')
-
-        # Listen for a photo message from the same user
-        user_message = await bot.listen(message.chat.id)  # Without filters argument
-        image_link = user_message.text.strip()
-
-        input_dir = await download_image_url(image_link)
-        thumb_dir = await process_image(input_dir, des_dir)
-
-        thumb = imgclient.upload(file=f"{thumb_dir}", expiration=3600)
-        await message.reply_text(
-                f"Thumbnail uploaded successfully!\n[View Thumbnail]({thumb['url']}) (Expires in 1 hour)",
-                disable_web_page_preview=True
-            )
-
-    except Exception as e:
-         await message.reply_text(f"Error in thumb gen {e}")
-    finally:
-            # Clean up the local thumbnail file
-            if os.path.exists(des_dir):
-                os.remove(des_dir)
-        
-async def process_image(input_dir, des_dir):
-    try:
-        async with aiofiles.open(input_dir, 'rb') as f:
-            content = await f.read()
-        input_image = Image.open(BytesIO(content))
-
-        # Resize the image to fit within 320x320 while maintaining aspect ratio
-        thumbnail_size = (320, 320)
-        input_image.thumbnail(thumbnail_size, Image.Resampling.LANCZOS)
-
-        # Create a new blank canvas with white background
-        canvas = Image.new('RGB', thumbnail_size, 'white')
-
-        # Center the resized image on the canvas
-        x_offset = (thumbnail_size[0] - input_image.width) // 2
-        y_offset = (thumbnail_size[1] - input_image.height) // 2
-        canvas.paste(input_image, (x_offset, y_offset))
-
-        # Save the resulting thumbnail to a file
-        await sync_to_async(canvas.save, des_dir, "JPEG")
-    except Exception as e:
-        logger.error(f"Image Processing Error: {e}")
-        return None
-    finally:
-        await aioremove(input_dir)
-    return des_dir
-
-async def download_image_url(url):
-    path = "Images/"
-    if not await aiopath.isdir(path):
-        await mkdir(path)
-    image_name = url.split('/')[-1]
-    des_dir = ospath.join(path, image_name)
-    async with aioClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                async with aiopen(des_dir, 'wb') as file:
-                    async for chunk in response.content.iter_chunked(1024):
-                        await file.write(chunk)
-                logger.info(f"Image Downloaded Successfully as {image_name}")
-            else:
-                logger.error(f"Failed to Download Image from {url}")
-    return des_dir
 
 async def main():
     await asyncio.create_task(process_queue())
